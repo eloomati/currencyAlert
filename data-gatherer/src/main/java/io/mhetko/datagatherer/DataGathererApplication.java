611c5b75001f455109eacfa571583666520ac974
mhetko <mateusz.hetko@autopay.pl>
2025-09-25 18:51:21 +0200

    Improving file structure and docker configuration

--- data-gatherer/src/main/java/io/mhetko/datagatherer/DataGathererApplication.java
+++ data-gatherer/src/main/java/io/mhetko/datagatherer/DataGathererApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrencyAlertApplication {
+public class DataGathererApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CurrencyAlertApplication.class, args);
+        SpringApplication.run(DataGathererApplication.class, args);
     }
 
 }
